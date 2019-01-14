d6f7b57d0b14a5d99c5ce017b7ac69f5f60a8ceb
Oktay Bekar <asameplayer@gmail.com>
2019-01-14 17:57:16 +0100

    Added GameState (with GameEntity ENUM) and ZweikampfManager

---
+++ src/sameplayer/zweikampf/plugin/Enums/GameStates.java
@@ -0,0 +1,16 @@
+package sameplayer.zweikampf.plugin.Enums;
+
+public enum GameStates {
+
+    WAIT_SERVER_REBOOT,
+    WAIT_QUEUE,
+    WAIT_COUNTDOWN_FIGHT,
+
+    RUN_PICK_KIT,
+    RUN_COUNTDOWN_FIGHT,
+    RUN_FIGHT,
+
+    REBOOT_COUNTDOWN_LOBBY,
+    REBOOT_SERVER;
+
+}
